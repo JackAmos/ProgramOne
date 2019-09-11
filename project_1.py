3933078d1fb8c667d61641f36ea1dd325e461a4d
Jack Amos <jack671guam@yahoo.com>
2019-09-10 19:09:10 -0600

    made it a func

--- project_1.py
+++ project_1.py
@@ -4,14 +4,28 @@
 #Project 1
 
 
-with open("Amatrix",'r') as file:
-	contents = file.read()
+def print_and_write(matrix):
 
-matrix_values = [int(x) for x in contents.split() if x.isdigit()]
+	with open(matrix,'r') as file:
+		contents = file.read()
 
-matrix_row = matrix_values.pop(0)
-matrix_column = matrix_values.pop(0)
+	matrix_values = [int(x) for x in contents.split()]
 
-print(matrix_row)
-print(matrix_column)
-print(matrix_values)
+	matrix_row = matrix_values.pop(0)
+	matrix_column = matrix_values.pop(0)
+
+	row_start = 0
+	row_end = matrix_column
+
+	file_name = "CS2300P1aAmos.out"+matrix[0]
+
+	with open(file_name,"w") as file:
+		
+
+	while row_start < matrix_row*matrix_column:
+		print(matrix_values[row_start:row_end])
+		row_start+=matrix_column
+		row_end+=matrix_column
+
+print_and_write("Amatrix")
+print_and_write("Bmatrix")
