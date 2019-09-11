9eb52793b1e42b7f49141a3a2ac2b249fae6893e
Jack Amos <jack671guam@yahoo.com>
2019-09-11 11:11:37 -0600

    Added subtract function

--- project_1.py
+++ project_1.py
@@ -31,5 +31,54 @@ def print_and_write(matrix):
 	with open(file_name,"w+") as file:
 		file.write(matrix_string)
 
+
+def subtract(matrix1,matrix2):
+	
+	with open(matrix1,'r') as file:
+		contents = file.read()
+
+	matrix_values1 = [int(x) for x in contents.split()]
+
+	matrix_row = matrix_values1.pop(0)
+	matrix_column = matrix_values1.pop(0)
+
+	with open(matrix2,'r') as file:
+		contents = file.read()
+
+	matrix_values2 = [int(x) for x in contents.split()]
+
+	matrix_row = matrix_values2.pop(0)
+	matrix_column = matrix_values2.pop(0)
+
+	index = 0
+	matrix_sub = []
+
+	for n in matrix_values1:
+		matrix_sub[index] = n - matrix_values2[index]
+		index+=1
+
+	row_start = 0
+	row_end = matrix_column
+	matrix_string = ""
+
+	while row_start < matrix_row*matrix_column:
+		matrix_string += str(matrix_sub[row_start:row_end]) + "\n"
+		row_start+=matrix_column
+		row_end+=matrix_column
+
+	print(matrix_string)
+
+	file_name = "CS2300P1aAmos.calc"
+
+	with open(file_name,"w+") as file:
+		file.write(matrix_string)
+
+
+
+
 print_and_write("Amatrix")
 print_and_write("Bmatrix")
+subtract("Amatrix","Bmatrix")
+
+
+
